cc19bf2a7492318b09cac1446e18ea3efd034d62
Peter <petershotbox@gmail.com>
2023-03-23 00:20:04 +0000

    New CSS

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 url = "https://code-checker.onrender.com/api/checker"
 async function getData() {
     try {
-      const response = await fetch(url, {
+        const response = await fetch(url, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
         }
       });
       const data = await response.json();
-      console.log("Updated")
+      console.log("Updated : ", await data.payload)
       return await data.payload;
     } catch (error) {
       document.querySelector("body").style.backgroundColor = 'red';
@@ -22,36 +22,42 @@ async function updateData() {
     
     // create new divs if necessary
     if (divs.length < data.length) {
-      for (let i = divs.length; i < data.length; i++) {
-        const div = document.createElement('div');
-        const idTag = document.createElement('p');
-        idTag.textContent = 'ID: ';
-        div.appendChild(idTag);
-        const statusTag = document.createElement('p');
-        statusTag.textContent = 'Status: ';
-        div.appendChild(statusTag);
-        const placeTag = document.createElement('p');
-        placeTag.textContent = 'Location: ';
-        div.appendChild(placeTag);
-        document.querySelector("body").appendChild(div);
-      }
-      divs = document.querySelectorAll('div');
+        for (let i = divs.length; i < data.length; i++) {
+            div = document.createElement('div');
+
+            const placeTag = document.createElement('p');
+            placeTag.textContent = '';
+            div.appendChild(placeTag);
+
+            const statusTag = document.createElement('p');
+            statusTag.textContent = 'Status: ';
+            div.appendChild(statusTag);
+            
+            const idTag = document.createElement('p');
+            idTag.textContent = 'ID: ';
+            div.appendChild(idTag);
+        
+            document.querySelector("body").appendChild(div);
+        }
+        divs = document.querySelectorAll('div');
     }
     
     // update existing divs
     data.forEach((item, index) => {
-      const [id, status, place] = item;
-      const idTag = divs[index].querySelector('p:first-of-type');
-      idTag.textContent = `ID: ${id}`;
-      const statusTag = divs[index].querySelector('p:nth-of-type(2)');
-      statusTag.textContent = `Status: ${status}`;
-      if (status === 'good') {
-        statusTag.style.color = 'green';
-      } else if (status === 'bad') {
-        statusTag.style.color = 'red';
-      }
-      const placeTag = divs[index].querySelector('p:last-of-type');
-      placeTag.textContent = `Location: ${place}`;
+        const [id, status, place] = item;
+        const placeTag = divs[index].querySelector('p:first-of-type');
+        placeTag.textContent = `${place}`;
+      
+        const statusTag = divs[index].querySelector('p:nth-of-type(2)');
+        statusTag.textContent = `Status: ${status}`;
+        if (status === 'good') {
+            statusTag.style.color = '#02bf02';
+        } else if (status === 'bad') {
+            statusTag.style.color = '#ba2222';
+        }
+        
+        const idTag = divs[index].querySelector('p:last-of-type');
+        idTag.textContent = `ID: ${id}`;
     });
   }
   
